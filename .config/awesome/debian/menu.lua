8d201ab7c53a2175a9bdea839ba4d7b9948c2546
vicky-sudo <kowavam119@nmailv.com>
2020-12-30 08:48:09 +0530

    add awesomeWM config

---
+++ .config/awesome/debian/menu.lua
@@ -0,0 +1,117 @@
+-- automatically generated file. Do not edit (see /usr/share/doc/menu/html)
+
+local awesome = awesome
+
+Debian_menu = {}
+
+Debian_menu["Debian_Applications_Accessibility"] = {
+	{"Xmag","xmag"},
+}
+Debian_menu["Debian_Applications_Editors"] = {
+	{"Xedit","xedit"},
+}
+Debian_menu["Debian_Applications_Graphics"] = {
+	{"X Window Snapshot","xwd | xwud"},
+}
+Debian_menu["Debian_Applications_Network_Communication"] = {
+	{"PuTTY","/usr/bin/putty","/usr/share/pixmaps/putty.xpm"},
+	{"Telnet", "x-terminal-emulator -e ".."/usr/bin/telnet.netkit"},
+	{"Xbiff","xbiff"},
+}
+Debian_menu["Debian_Applications_Network"] = {
+	{ "Communication", Debian_menu["Debian_Applications_Network_Communication"] },
+}
+Debian_menu["Debian_Applications_Programming"] = {
+	{"GDB", "x-terminal-emulator -e ".."/usr/bin/gdb"},
+	{"Tclsh8.6", "x-terminal-emulator -e ".."/usr/bin/tclsh8.6"},
+	{"TkWish8.6","x-terminal-emulator -e /usr/bin/wish8.6"},
+}
+Debian_menu["Debian_Applications_Science_Mathematics"] = {
+	{"Bc", "x-terminal-emulator -e ".."/usr/bin/bc"},
+	{"Dc", "x-terminal-emulator -e ".."/usr/bin/dc"},
+	{"Xcalc","xcalc"},
+}
+Debian_menu["Debian_Applications_Science"] = {
+	{ "Mathematics", Debian_menu["Debian_Applications_Science_Mathematics"] },
+}
+Debian_menu["Debian_Applications_Shells"] = {
+	{"Bash", "x-terminal-emulator -e ".."/bin/bash --login"},
+	{"Dash", "x-terminal-emulator -e ".."/bin/dash -i"},
+	{"Sh", "x-terminal-emulator -e ".."/bin/sh --login"},
+}
+Debian_menu["Debian_Applications_System_Administration"] = {
+	{"Debian Task selector", "x-terminal-emulator -e ".."su-to-root -c tasksel"},
+	{"DSL/PPPoE configuration tool", "x-terminal-emulator -e ".."/usr/sbin/pppoeconf","/usr/share/pixmaps/pppoeconf.xpm"},
+	{"Editres","editres"},
+	{"Logout (obsession)","/usr/bin/obsession-logout"},
+	{"pppconfig", "x-terminal-emulator -e ".."su-to-root -p root -c /usr/sbin/pppconfig"},
+	{"Ubiquity Installer","/usr/bin/ubiquity","/usr/share/pixmaps/ubiquity.xpm"},
+	{"Xclipboard","xclipboard"},
+	{"Xfontsel","xfontsel"},
+	{"Xkill","xkill"},
+	{"Xrefresh","xrefresh"},
+}
+Debian_menu["Debian_Applications_System_Hardware"] = {
+	{"Xvidtune","xvidtune"},
+}
+Debian_menu["Debian_Applications_System_Monitoring"] = {
+	{"Pstree", "x-terminal-emulator -e ".."/usr/bin/pstree.x11","/usr/share/pixmaps/pstree16.xpm"},
+	{"Top", "x-terminal-emulator -e ".."/usr/bin/top"},
+	{"Xconsole","xconsole -file /dev/xconsole"},
+	{"Xev","x-terminal-emulator -e xev"},
+	{"Xload","xload"},
+}
+Debian_menu["Debian_Applications_System_Package_Management"] = {
+	{"Aptitude Package Manager (text)", "x-terminal-emulator -e ".."/usr/bin/aptitude-curses"},
+	{"Synaptic Package Manager","x-terminal-emulator -e synaptic-pkexec","/usr/share/synaptic/pixmaps/synaptic_32x32.xpm"},
+}
+Debian_menu["Debian_Applications_System_Security"] = {
+	{"light-locker-settings","light-locker-settings"},
+}
+Debian_menu["Debian_Applications_System"] = {
+	{ "Administration", Debian_menu["Debian_Applications_System_Administration"] },
+	{ "Hardware", Debian_menu["Debian_Applications_System_Hardware"] },
+	{ "Monitoring", Debian_menu["Debian_Applications_System_Monitoring"] },
+	{ "Package Management", Debian_menu["Debian_Applications_System_Package_Management"] },
+	{ "Security", Debian_menu["Debian_Applications_System_Security"] },
+}
+Debian_menu["Debian_Applications_Viewers"] = {
+	{"Shotwell","/usr/bin/shotwell"},
+	{"Xditview","xditview"},
+}
+Debian_menu["Debian_Applications"] = {
+	{ "Accessibility", Debian_menu["Debian_Applications_Accessibility"] },
+	{ "Editors", Debian_menu["Debian_Applications_Editors"] },
+	{ "Graphics", Debian_menu["Debian_Applications_Graphics"] },
+	{ "Network", Debian_menu["Debian_Applications_Network"] },
+	{ "Programming", Debian_menu["Debian_Applications_Programming"] },
+	{ "Science", Debian_menu["Debian_Applications_Science"] },
+	{ "Shells", Debian_menu["Debian_Applications_Shells"] },
+	{ "System", Debian_menu["Debian_Applications_System"] },
+	{ "Viewers", Debian_menu["Debian_Applications_Viewers"] },
+}
+Debian_menu["Debian_Games_Toys"] = {
+	{"Oclock","oclock"},
+	{"Xclock (analog)","xclock -analog"},
+	{"Xclock (digital)","xclock -digital -update 1"},
+	{"Xeyes","xeyes"},
+	{"Xlogo","xlogo"},
+}
+Debian_menu["Debian_Games"] = {
+	{ "Toys", Debian_menu["Debian_Games_Toys"] },
+}
+Debian_menu["Debian_Help"] = {
+	{"Xman","xman"},
+}
+Debian_menu["Debian_Window_Managers"] = {
+	{"awesome",function () awesome.exec("/usr/bin/awesome") end,"/usr/share/pixmaps/awesome.xpm"},
+}
+Debian_menu["Debian"] = {
+	{ "Applications", Debian_menu["Debian_Applications"] },
+	{ "Games", Debian_menu["Debian_Games"] },
+	{ "Help", Debian_menu["Debian_Help"] },
+	{ "Window Managers", Debian_menu["Debian_Window_Managers"] },
+}
+
+debian = { menu = { Debian_menu = Debian_menu } }
+return debian
